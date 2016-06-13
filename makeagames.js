02a7649618f6ac17cf65dcbe78580f544dc2443a
Fahrur Rifai <faimaknyus@gmail.com>
2016-06-13 14:38:39 +0700

    added counter and more

--- makeagames.js
+++ makeagames.js
@@ -9,17 +9,21 @@ $.fn.makeAGames = function(options){
 	$this = $(this);
 
 	var defaults = {
-		'cols'  			: 4, // 3 for now
-		'random'			: 100,
+		'cols'  		: 4, // 3 for now
+		'random'		: 100,
 		'imageLink' 		: $this.data('image'),
 		'animation' 		: true,
 		'contentBox'		: "{i}",
-		'onGameOver' 		: function(timer){ alert('Complete in ' + timer + ' seconds.'); },
+		'onGameOver' 		: function(timer, step){ alert('Complete in ' + timer + ' seconds and ' + step + ' steps.'); },
 		'containerBorder'	: 1,
 		'sliceBorder'		: 1,
 		'borderColor'		: '#222',
 		'borderType'		: 'solid',
-		'timerBox'  		: $('#mag-timer')
+		'timerBox'  		: $('#mag-timer'),
+		'stepBox'		: $('#mag-step'),
+		'keyboardPlay'		: false,
+		'keyboardCode'		: {"up": 38, "down": 40, "right": 39, "left": 37},
+		'keyboardReverse'	: false
 	};
 
 	option = (typeof options == 'undefined') ? {} : options;
@@ -27,9 +31,11 @@ $.fn.makeAGames = function(options){
 	var setting = $.extend(defaults, option);
 
 	imgames = new Image();
+
 	imgames.onerror = function() {
-		show_("Image is not Found.", $this);
+		$this.show_("Image is not Found.", $this);
 	};
+	
 	imgames.onload = function() {
 		// set height and width
 		imageWidth = imgames.width;
@@ -39,17 +45,149 @@ $.fn.makeAGames = function(options){
 		// column size
 		colSize = Math.round(imageWidth/setting.cols);
 
+		$this.cssify(containerSize,containerSize,colSize,colSize,setting.imageLink,setting.containerBorder,setting.sliceBorder,setting.borderType,setting.borderColor);
+
+		$this.buildColumn();
+
+		// randoming
+		$this.randomize(setting.random);
+
+		// timer,step = 0
+		$this.countTo(timer);
+		$this.stepTo(step);
+
+		$('.mag-slice').click(function(){
+
+			if ( ! gameOver) {
+
+				if ($this.moveTo($(this), $('.mag-slice.mag-blank')) != false) {
+
+					if ( ! startTimer) {
+						startTimer = true;
+						// start counting
+						countdown = setInterval($this.counting, 10);
+					}
+
+					// steping
+					$this.stepping();
+					
+					if ($this.checkIfComplete()) {
+						// stop the game
+						$this.stopGame();
+					};
+				};
+			};
+			return false;
+		});
+
+		if (setting.keyboardPlay) {
+			// reverse key
+			if (setting.keyboardReverse) {
+				dump = setting.keyboardCode;
+				setting.keyboardCode = {"up": dump.down, "down": dump.up, "right": dump.left, "left": dump.right};
+				delete dump;
+			}
+
+			$(document).keydown(function(e){
+				var key = e.which || e.keyCode || 0;
+				var blanked = $('.mag-slice.mag-blank');
+				var blanked_i = parseInt(blanked.attr('mag-i'));
+				if(key == setting.keyboardCode.up){
+					var move_to = (blanked_i-setting.cols) > 0 ? blanked_i-setting.cols : false;
+				} else if(key == setting.keyboardCode.right) {
+					var move_to = (blanked_i % setting.cols) != 0 ? blanked_i+1 : false; 
+				} else if(key == setting.keyboardCode.down) {
+					var move_to = (blanked_i+setting.cols) <= (setting.cols*setting.cols) ? (blanked_i+setting.cols) : false;
+				} else if(key == setting.keyboardCode.left) {
+					var move_to = ((blanked_i-1) % setting.cols) != 0 ? blanked_i-1 : false;
+				} else {
+					var move_to = false;
+				}
+				
+
+				if (move_to) {
+					
+					if ( ! gameOver) {
+
+						if ($this.moveTo(blanked, $('.mag-slice[mag-i='+move_to+']')) != false) {
+
+							if ( ! startTimer) {
+								startTimer = true;
+								// start counting
+								countdown = setInterval($this.counting, 10);
+							}
+
+							// steping
+							$this.stepping();
+							
+							if ($this.checkIfComplete()) {
+								// stop the game
+								$this.stopGame();
+							};
+						};
+					};
+
+					return false;
+				}
+			});
+		}
+
+	};
+
+	// image from url
+	imgames.src = setting.imageLink;
+
+	$this.cssify = function(w,h,cw,ch,i,bzc,bzs,bs,bc){
 		// create base container for slices image
-		$this.css({width: containerSize, height: containerSize, border: setting.containerBorder + 'px ' + setting.borderType + ' ' + setting.borderColor });
+		$this.css({width: w, height: h, border: bzc + 'px ' + bs + ' ' + bc });
+
+		$this.addClass('makeagames_wrapper');
 
 		// adding the column style
-		addStyle('.mag-slice {width:' + colSize + 'px; height:' + colSize + 'px; border: ' + setting.sliceBorder + 'px ' + setting.borderType + ' ' + setting.borderColor + '}');
-		addStyle('.mag-bg {background-image: url(\'' + setting.imageLink + '\')}');
+		$this.addStyle('.mag-slice {width:' + cw + 'px; height:' + ch + 'px; border: ' + bzs + 'px ' + bs + ' ' + bc + '}');
+		$this.addStyle('.mag-bg {background-image: url(\'' + i + '\')}');
+	}
 
+	$this.countTo = function(n){
+		n = (n/100).toFixed(2);
+		$(setting.timerBox).text(n);
+	}
+
+	$this.stepTo = function(n){
+		// show in step box
+		$(setting.stepBox).text(n);
+	}
+	
+	$this.stepping = function(){
+		step++;
+		$this.stepTo(step);
+	}
+	
+	$this.counting = function(){
+		timer++;
+		$this.countTo(timer);
+	}
+
+	$this.stopCounting = function(){
+		// stop counting
+		clearInterval(countdown);
+	}
+
+	$this.stopGame = function(){
+		// stop counting
+		$this.stopCounting();
+		// call onGameOver function
+		timer = (timer/100).toFixed(2);
+		setting.onGameOver.call($this, timer, step);
+		// disable the click event
+		gameOver = true;
+	}
+
+	$this.buildColumn = function(){
 		// adding the column
 		for (var i = 1; i <= (setting.cols*setting.cols); i++) {
 			box_content = setting.contentBox ? setting.contentBox.replace("{i}", i) : '';
-			addCol('<div mag-i="' + i + '" mag-p="' + i + '" class="mag-slice mag-bg mag-i-n-' + i + ' mag-bg-n-' + i + '">' + box_content + '</div>', $this);
+			$this.addCol('<div mag-i="' + i + '" mag-p="' + i + '" class="mag-slice mag-bg mag-i-n-' + i + ' mag-bg-n-' + i + '">' + box_content + '</div>', $this);
 			currentRow = Math.floor((i-1)/setting.cols);
 			currentCol = ((i % setting.cols == 0 ? setting.cols : i % setting.cols)-1);
 			leftN = currentCol*colSize;
@@ -58,8 +196,8 @@ $.fn.makeAGames = function(options){
 			topPosition = topN+((currentRow)*2)+setting.containerBorder;
 			xBg = leftN;
 			yBg = topN;
-			addStyle('.mag-i-n-' + i + '{top:' + topPosition + 'px; left:' + leftPosition + 'px}');
-			addStyle('.mag-bg-n-' + i + '{background-position-y:' + -yBg + 'px; background-position-x:' + -xBg + 'px}');
+			$this.addStyle('.mag-i-n-' + i + '{top:' + topPosition + 'px; left:' + leftPosition + 'px}');
+			$this.addStyle('.mag-bg-n-' + i + '{background-position-y:' + -yBg + 'px; background-position-x:' + -xBg + 'px}');
 			
 			// set the rule
 			rule = [];
@@ -85,116 +223,85 @@ $.fn.makeAGames = function(options){
 
 		// remove the last slice
 		$('.mag-i-n-' + (setting.cols*setting.cols)).removeClass('mag-bg').addClass('mag-blank');
+	}
 
-		// randoming
-		var r_n_b = (setting.cols*setting.cols);
+	$this.randomize = function(rand){
+		var r_n_b = $this.find('.mag-blank').attr('mag-p');
 		var r_n_b_r = rules[r_n_b];
 		var r_n_b_r_o = r_n_b_r[Math.floor(Math.random() * r_n_b_r.length)];
-		for (var i = 1; i <= setting.random; i++) {
-			moveTo($('.mag-slice.mag-i-n-' + r_n_b_r_o), $('.mag-slice.mag-blank'), {rules: rules});
+		for (var i = 1; i <= rand; i++) {
+			$this.moveTo($('.mag-slice.mag-i-n-' + r_n_b_r_o), $('.mag-slice.mag-blank'));
 			r_n_b_r = rules[r_n_b_r_o];
 			r_n_b_r_o = r_n_b_r[Math.floor(Math.random() * r_n_b_r.length)];
 		};
+	}
 
-		$('.mag-slice').click(function(){
-			// step
-			step++;
+	$this.checkIfComplete = function() {
+		var countslice = $('.mag-slice').length;
+		var countbyone = 0;
+		$('.mag-slice').each(function(_k,_v){
+			// current position
+			var cup = $(_v).attr('mag-i');
+			// should position
+			var shup = $(_v).attr('mag-p');
 
-			if ( ! startTimer) {
-				startTimer = true;
-				// start counting
-				countdown = setInterval(counting, 1000);
+			if (cup == shup) {
+				countbyone++;
 			}
-			if ( ! gameOver) {
-				moveTo($(this), $('.mag-slice.mag-blank'), {rules: rules});
-				if (checkIfComplete()) {
-					// stop counting
-					clearInterval(countdown);
-					// call onGameOver function
-					setting.onGameOver.call($this, timer, step);
-					// disable the click event
-					gameOver = true;
-				};
-			};
-			return false;
 		});
-	};
 
-	// image from url
-	imgames.src = setting.imageLink;
-
-	// timer 0
-	$(setting.timerBox).text(0);
-	
-	function counting(){
-		timer++;
-		$(setting.timerBox).text(timer);
+		if (countbyone == countslice) {
+			return true;
+		} else {
+			return false;
+		};
 	}
 
-}
+	$this.restartGame = function(){
+		$this.stopCounting();
+		$this.randomize(setting.random);
 
-function show_(errormsg,element,replace){
-	if (typeof replace == 'undefined') { replace = false; } else { replace = replace; };
-	if (replace) { $(element).html(errormsg); } else { $(element).append(errormsg); };
-}
-
-function addCol(content, to) {
-	show_(content, to, false);
-}
-
-function addStyle(css) {
-	if ( ! $('style#makeagames-style').length) {
-		show_('<style id="makeagames-style" type="text/css"></style>', $('head'));
+		startTimer = false;
+		timer = 0;
+		step = 0;
+		$this.countTo(timer);
+		$this.stepTo(step);
+		gameOver = false;
 	}
-	show_(css, $('style#makeagames-style'));
-}
 
-function moveTo(from, to, someoption){
-	var defaultopt = {
-		'rules' 			: [], 
-		'uniqueAttr'		: 'mag-i',
-		'classReverse'		: 'mag-i-n-'
-	};
-	myoption = (typeof someoption == 'undefined') ? {} : someoption;
-	var mysetting = $.extend(defaultopt, myoption);
-
-	var clicked = parseInt($(to).attr(mysetting.uniqueAttr));
-	var blanked = $(from).attr(mysetting.uniqueAttr);
-	var moveable = $.inArray(clicked, mysetting.rules[blanked]) >= 0 ? true : false ;
-	if (moveable) {	
-		$(to).attr(mysetting.uniqueAttr, blanked).removeClass(mysetting.classReverse + clicked).addClass(mysetting.classReverse + blanked);
-		$(from).attr(mysetting.uniqueAttr, clicked).removeClass(mysetting.classReverse + blanked).addClass(mysetting.classReverse + clicked);
-	}
-}
-
-function randomize(blank, some_o) {
-	var default_o = {
-		'rules' 			: [], 
-		'uniqueAttr'		: 'mag-i',
-		'classReverse'		: 'mag-i-n-'
-	};
-	my_o = (typeof some_o == 'undefined') ? {} : some_o;
-	var my_s = $.extend(default_o, my_o);
-}
-
-function checkIfComplete()
-{
-	var countslice = $('.mag-slice').length;
-	var countbyone = 0;
-	$('.mag-slice').each(function(_k,_v){
-		// current position
-		var cup = $(_v).attr('mag-i');
-		// should position
-		var shup = $(_v).attr('mag-p');
-
-		if (cup == shup) {
-			countbyone++;
+	$this.moveTo = function(from, to){
+		var mysetting = {
+			'uniqueAttr'		: 'mag-i',
+			'classReverse'		: 'mag-i-n-'
 		}
-	});
 
-	if (countbyone == countslice) {
-		return true;
-	} else {
-		return false;
-	};
+		var clicked = parseInt($(to).attr(mysetting.uniqueAttr));
+		var blanked = $(from).attr(mysetting.uniqueAttr);
+		var moveable = $.inArray(clicked, rules[blanked]) >= 0 ? true : false ;
+		if (moveable) {	
+			$(to).attr(mysetting.uniqueAttr, blanked).removeClass(mysetting.classReverse + clicked).addClass(mysetting.classReverse + blanked);
+			$(from).attr(mysetting.uniqueAttr, clicked).removeClass(mysetting.classReverse + blanked).addClass(mysetting.classReverse + clicked);
+		} else {
+			return false;
+		}
+	}
+
+	$this.show_ = function(errormsg,element,replace){
+		if (typeof replace == 'undefined') { replace = false; } else { replace = replace; };
+		if (replace) { $(element).html(errormsg); } else { $(element).append(errormsg); };
+	}
+
+	$this.addStyle = function(css) {
+		if ( ! $('style#makeagames-style').length) {
+			$this.show_('<style id="makeagames-style" type="text/css"></style>', $('head'));
+		}
+		$this.show_(css, $('style#makeagames-style'));
+	}
+
+	$this.addCol = function(content, to) {
+		// column slice
+		$this.show_(content, to, false);
+	}
+
+	return $this;
 }
